b00ba3ac9b8dccfacef6c92ff4b9d631db3335fa
Romulo De Lazzari <romulodelazzari@gmail.com>
2013-05-14 19:52:38 +0100

    add tests and a new xml parser

---
+++ src/Silverpop/Util/Serializer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Silverpop\Util;
+
+use LSS\Array2XML,
+    LSS\XML2Array;
+
+class Serializer
+{
+  private $default_root;
+
+  public function __construct($default_root=null) {
+    if(is_null($default_root)) $this->default_root = "Envelope";
+    else $this->default_root = $default_root;
+  }
+
+  public function serialize(array $array) {
+    // Array -> XML
+    return Array2XML::createXML($this->default_root, $array)->saveXML();
+  }
+
+  public function unserialize($xml) {
+    // XML -> Array
+    return XML2Array::createArray($xml);
+  }
+}
